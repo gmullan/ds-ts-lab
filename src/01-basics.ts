ff866c4cfd7e70557654fb9508c35e78d1c49fa9
Gemma Mullan <20083580@setu.ie>
2026-09-16 11:33:34 +0100

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -15,6 +15,12 @@ interface Colleague {
     }
 }
 
+interface ColleagueHistory {
+  current: Colleague[],
+  former: Colleague[]
+}
+
+
 const friend1 = {
   name: "Paul Fleming",
   phone: "087-12345",
@@ -57,7 +63,7 @@ const colleague3 = {
     extension: 125,
   },
 };
-const colleagues = {
+export const colleagues: ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
