a39103a700d2a7d3fac9cc278810f8764614126b
Gemma Mullan <20083580@setu.ie>
2026-09-19 17:13:31 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -47,9 +47,12 @@ console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 function sortColleagues(
   colleagues: Colleague[],
   sorter: (c1: Colleague, c2: Colleague) => number,
-  max : number
+  max? : number
 ): EmailContact[] {
-  const end = max < 2 ? 1 : max
+  let end = colleagues.length;
+  if (max !== undefined) {
+     end = max < 2 ? 1 : max
+  }
   const sorted = colleagues.sort(sorter);
   const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
   return fullResult.slice(0,end)
@@ -57,6 +60,8 @@ function sortColleagues(
 // Test invocations
 console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
+
 
 function findFriends(
   fs: Friend[],
@@ -68,3 +73,18 @@ function findFriends(
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(
+  friend: Friend,
+  interest: string
+): string[] {
+  if (friend.interests === undefined) {
+    friend.interests = [];
+  }
+
+  friend.interests.push(interest);
+
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'));
