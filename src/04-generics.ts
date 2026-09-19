46ce6b624784784e18defb04c990aaa13cd5a75b
Gemma Mullan <20083580@setu.ie>
2026-09-19 17:34:33 +0100

    Define types using Utility types.

--- src/04-generics.ts
+++ src/04-generics.ts
@@ -8,16 +8,6 @@ function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined
 console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
 console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
 
-import { friends, colleagues } from './01-basics'
-import { Friend, Colleague } from './myTypes'
-
-function findMatch<T>(
-  data: T[],
-  criteria: (d: T) => boolean
-): T | undefined {
-  return data.find(criteria)
-}
-
 console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')))
 console.log(
   findMatch<Colleague>(
